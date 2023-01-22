d806b14f8dd4fe143e609d7f8647594bae8be268
Yorman Eduardo Amaro Carrero <yormaneduardoamarocarrero@yormans-mbp.lan>
2023-01-22 15:03:48 -0300

    instalacion de dependencias necesarias y creacion de files para manejo de peticiones a la api

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -1,1 +1,13 @@
 // aqui se van a orquestar nuestras peticiones a los empoint, a las rutas que tenga el api.
+// Se instalaran una libreria de peticiones (axios)
+
+import axios from "axios"; // luego de ya instalado se importa
+import { BASE_URL, API_KEY_PARAM } from "../config";
+
+export class TVShowAPI {
+
+    // Axios
+    static async
+
+}
+
