9df01c431a20f7ed2cc4bc6ed3766633790ca730
Yorman Eduardo Amaro Carrero <yormaneduardoamarocarrero@yormans-mbp.lan>
2023-01-26 13:03:07 -0300

    Adding comments to the project

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,14 @@ import s from "./style.module.css";
 function App() {
   const [currentTVShow, setCurrentTVShow] = useState(); // Aqui se crean propiedades para manejar el estado de la serie mas popular recuerden que va cambiando por popularidad se llamara (currentTVShow) 
 
-  async function fetchPopulars() {
+  async function fetchPopulars() { // Estamos haciendo uso de la clase fetchPopulars creada en el archivo de tv-show.js de la api que la dejamos exportada con el nombre de (TVShowAPI). 
     const popularTVShowList = await TVShowAPI.fetchPopulars(); // Vamos almacenar el resultado de la peticion y el await para esperar que nos responda la peticion
     if (popularTVShowList && popularTVShowList.length > 0) {  // Se pregunta si la lista es mayor a 0 su cantidad en concreto
-      setCurrentTVShow(popularTVShowList[0]); // y si es mayor a 0 actualizar el mas popular y mostrar el primero
+      setCurrentTVShow(popularTVShowList[0]); // y si es mayor a 0 actualizar el mas popular y mostrar el primero, para actualizar su modificador setcurrentTVShow
     }
   }
 
-  console.log(currentTVShow); // Para ver donde esta ubicada esa imagen 
+  console.log(currentTVShow); // Para ver donde esta ubicada esa imagen dentro del contexto del objeto.
 
   useEffect(() => { // Es necesario llamar a useffect ya que esa serie popular va a cambiar nunca sera la misma ya que si otra tiene mas puntaje sera esa y asi...
     fetchPopulars();
@@ -26,15 +26,15 @@ function App() {
 
   return (
     <div
-    className={s.main_container}
+    className={s.main_container} 
     style={{
-      background: currentTVShow
-        ? `linear-gradient(rgba(0, 0, 0, 0.100), rgba(0, 0, 0, 0.100)),
+      background: currentTVShow // Va a estar situado en un constexto de si tenemos informacion del currentTVShow
+        ? `linear-gradient(rgba(0, 0, 0, 0.100), rgba(0, 0, 0, 0.100)), 
           url("${BACKDROP_BASE_URL}${currentTVShow.backdrop_path}") no-repeat center / cover` 
         : "black",  
-    }}
+    }} // Se realizo un operador ternario para mostrar o no la imagen del currentTVShow
     >
-      <div className={s.header}>
+      <div className={s.header}> 
         <div className="row">
           <div className="col-4">
             <Logo 
