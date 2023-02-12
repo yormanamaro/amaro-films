6610263909efabc97d68140a981f2b12b1a59eee
Yorman Eduardo Amaro Carrero <yormaneduardoamarocarrero@yormans-mbp.lan>
2023-02-12 20:02:38 -0300

    vite confing file

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,24 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  // OPCIONES DE SERVIDOR Y PREVIU
+  // OPCIONES DE SERVIDOR Y PREVIU // MONITOREOS PREVIOS
   server: { // Para cambiar el servidor local por otro puerto
     port: 3030,
   },
-  preview: { // Para desplegar un servidor pre-productivo
+  preview: { // Para desplegar un servidor pre-productivo // antes de generar este script hay que generar el build
     port: 4270,
-  }
+  },
+
+  // Para personalizar el objeto del build // procesos vinculados para produccion (para personalizar el dist)
+  build: {
+    incremental: true, // Acelerar el proceso de compilacion de los archivos cuando generas build
+    babel: {  // Habilitar un trabajo en conjunto con babel del versionado de JS para manejo correcto a la version que necesite el navegador
+      presets: ["@babel/preset-env", "@babel/preset-react"],
+    }, 
+    /*typescript: { //Habilitar la aceleracion de compilacion de tu codigo TS hacia js
+      tsconfig: "./tsconfig.json",
+    },*/
+    cache: true, // Habilitar una cache para optimizar el compilado de los recursos que caen en dist
+    minity: true, // Habilitar la opcion de compresion optimizada para minimizar el tamano de los archivos generados.
+  }  
 });
